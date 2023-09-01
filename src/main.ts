a93c8016c58b2ba44fc0827710b5a1691b70ddbf
Kayque Prado Silva <46896994+kayqueprado580@users.noreply.github.com>
2023-09-01 18:41:18 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ConfigService } from '@nestjs/config';
+
 // import { PrismaService } from './database/prisma.service';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   // Configuração do CORS para permitir "localhost"
+  const configService = app.get(ConfigService);
+  const allowedOrigins: string[] = [configService.get('ALLOWED_ORIGIN', 'http://localhost:3000')];
+  const corsOptions: CorsOptions = {
+    // origin: true,
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+  app.enableCors(corsOptions);
 
   app.setGlobalPrefix('v1');
   app.useGlobalPipes(new ValidationPipe());
